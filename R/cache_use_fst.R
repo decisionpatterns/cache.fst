940f6c098cea75c226d0ed5e8e162e0c625aea4b
Christopher Brown <cbrown@decisionpatterns.com>
2018-12-18 04:58:38 -0800

    Update

---
+++ R/cache_use_fst.R
@@ -0,0 +1,43 @@
+extension = 'fst'
+
+#' Cache objects using fst (fst)
+#'
+#' Enables caching using the *fst* package
+#'
+#' @details
+#'
+#' You probably should use [cache_use_fst()] instead which makes fst
+#' the default method for reading/writing to the cache.  There is generally no
+#' reason to invoke this function as it is called when the packages is
+#' attached.
+#'
+#' @seealso
+#'  - [cache_use_fst()]
+#'
+#' @examples
+#' dontrun{
+#'  cache_use_fst()
+#'  cache(mtcars)
+#'  if( exists('mtcars') ) rm(mtcars)
+#'  uncache(mtcars)
+#'  fs::dir_ls( cache_find() )
+#' }
+#'
+#' @importFrom cache cache_backend_register
+#' @rdname cache.fst
+#' @export
+
+cache_register_fst <- function()
+  cache::cache_backend_register(
+      name='fst'
+    , reader=fst::read_fst
+    , writer=fst::write_fst
+    , ext=extension
+    )
+
+
+#' @rdname cache.fst
+#' @export
+
+cache_use_fst <- function()
+  cache::cache_backend("fst")
